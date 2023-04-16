dae77d72ca04c567e4b4959198b987fff4c289aa
maxivega0 <maximilianovega600@gmail.com>
2023-04-16 04:41:52 -0300

    logica cronometro completa

--- js/app.js
+++ js/app.js
@@ -1,24 +1,37 @@
-
 let horas = 0,
 minutos = 0,
 segundos = 0,
-bandera = false;
+bandera = false,
+idInterval;
 
-let cronometro = document.querySelector("#cronometro"),
-botonIniciar = document.querySelector("#btninicio"),
+let cronometro = document.getElementById("cronometro"),
+botonIniciar = document.querySelector("#btniniciar"),
 botonPausar = document.querySelector("#btnPausar"),
-BotonDetener = document.querySelector("#btnDetener");
+botonDetener = document.querySelector("#btnDetener");
 
 botonIniciar.addEventListener('click', comenzarCronometro);
-// botonPausar.addEventListener('click', pausarCronometro);
-// botonDetener.addEventListener('click', detenerCronometro);
+botonPausar.addEventListener('click', pausarCronometro);
+botonDetener.addEventListener('click', detenerCronometro);
 
 function comenzarCronometro() {
     if (bandera === false) {
-        cronometro = setInterval(actualizarCronometro, 1000);
+        idInterval = setInterval(actualizarCronometro, 1000);
         bandera = true;
-        console.log("hola");
     }
+}
+function pausarCronometro() {
+    if (bandera) {
+        clearInterval(idInterval);
+        bandera = false;
+    }
+}
+function detenerCronometro() {
+    clearInterval(idInterval);
+    bandera = false;
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    cronometro.innerHTML = `0${horas}:0${minutos}:0${segundos}`;
 }
 function actualizarCronometro() {
     segundos++;
@@ -30,12 +43,8 @@ function actualizarCronometro() {
         minutos = 0;
         horas++;
     }
-    if(segundos<10){
-        segundos = '0'+segundos;
-    }
-
-    console.log(segundos);
-    const tiempo = `${horas}:${minutos}:${segundos}`;
-    console.log(tiempo);
-    cronometro.innerHTML = tiempo;
+    let horasString = horas < 10 ? `0${horas}` : horas.toString(),
+    minutosString = minutos < 10 ? `0${minutos}` : minutos.toString(),
+    segundosString = segundos < 10 ? `0${segundos}` : segundos.toString();
+    cronometro.innerHTML = `${horasString}:${minutosString}:${segundosString}`;
 }
